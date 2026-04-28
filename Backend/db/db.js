46aea629929b792f830e031368c98f0731fb12e6
Grace Christina Rithika <gracechristinarithika@Christinas-MacBook-Air.local>
2026-04-28 12:00:23 +0530

    added env file

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 module.exports = () => {
-	return mongoose.connect("mongodb+srv://tina:2303@cluster0.g89ldyv.mongodb.net/charmingstore");
+	return mongoose.connect(process.env.REACT_MONGO_URL);
 };
